79efd74f84c8b8a9cde483551dfef6578236a14f
Travelintrips <divatranssoetta@gmail.com>
2026-08-27 16:13:23 +0000

    latest application updates

--- replit.nix
+++ replit.nix
@@ -1,5 +1,27 @@
 { pkgs }: {
   deps = [
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXext
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libxcb
+    pkgs.xorg.libX11
+    pkgs.mesa
+    pkgs.libxkbcommon
+    pkgs.alsa-lib
+    pkgs.cairo
+    pkgs.pango
+    pkgs.gtk3
+    pkgs.libdrm
+    pkgs.expat
+    pkgs.dbus
+    pkgs.cups
+    pkgs.at-spi2-atk
+    pkgs.atk
+    pkgs.nspr
+    pkgs.nss
+    pkgs.glib
     pkgs.azure-functions-core-tools
   ];
 }
